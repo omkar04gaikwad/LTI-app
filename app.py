23dfc03396e3992d535ae706d9ad2088710e536b
omkar04gaikwad <58172164+omkar04gaikwad@users.noreply.github.com>
2025-02-10 03:41:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = "rahulwmg767@gmail.com"
-app.config['MAIL_PASSWORD'] = "Omkar@123"
+app.config['MAIL_PASSWORD'] = "<password>"
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 
